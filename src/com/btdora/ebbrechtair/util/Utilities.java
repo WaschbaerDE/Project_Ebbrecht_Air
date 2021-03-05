880ee9fea273998b774d872f3132615e4894e2d4
Adrian <adrianpokemon@live.de>
2021-03-05 11:04:27 +0100

    Editing of the Check classes

--- src/com/btdora/ebbrechtair/util/Utilities.java
+++ src/com/btdora/ebbrechtair/util/Utilities.java
@@ -1,30 +1,56 @@
 package com.btdora.ebbrechtair.util;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utilities {
-    private String input;
-    private String outputString;
+
 
     public String getIcaoByName(String input) {
+         String outputString = "";
 
-        this.input = input;
         SQLConnector createSQLConnector = new SQLConnector();
 
         try (Statement stmt = createSQLConnector.getSQLConnection().createStatement();) {
-            String SQL = "SELECT * FROM db_Airport WHERE ICAOCode LIKE '" + this.input + "'";        // SELECT-ABFRAGE
+            String SQL = "SELECT * FROM db_Airport WHERE ICAOCode LIKE '" + input + "'";        // SELECT-ABFRAGE
             ResultSet rs = stmt.executeQuery(SQL);
             if (rs.next() == false) {
-                CheckIfStringIsValidAirportName checkIfStringIsValidAirportName = new CheckIfStringIsValidAirportName();
-                checkIfStringIsValidAirportName.CheckIfStringIsValidAirportName(this.input);
+               outputString = getAirportByName(input);
             }
             else{
-                this.outputString = this.input;     //Hier wird aus dem ICAO Code eine Airport generiert. Methode muss noch geschrieben werden;
+                outputString = input;     //Hier wird aus dem ICAO Code eine Airport generiert. Methode muss noch geschrieben werden;
             }
         }
         catch(SQLException e){
                 e.printStackTrace();
             }
-        return this.outputString;
+        return outputString;
+    }
+
+
+
+    public String getAirportByName(String input) {
+        List<Airport> list = new ArrayList<Airport>();
+        String outputString;
+        SQLConnector createSQLConnector = new SQLConnector();
+
+        try (Statement stmt = createSQLConnector.getSQLConnection().createStatement();) {
+            String SQL = "SELECT * FROM db_Airport WHERE AirportName LIKE '%" + input + "%'";        // SELECT-ABFRAGE
+            ResultSet rs = stmt.executeQuery(SQL);
+            if (rs.next() ==  false) {
+                System.out.println("Wir konnten keinen Flughafen mit Ihrer Eingabe finden. Bitte geben Sie einen valide Eingabe ein.");
+            }
+            else{
+                while(rs.next()){
+                    list.add(new Airport(rs.getString(2),rs.getString(3),rs.getInt(4),rs.getString(5),rs.getString(6),rs.getInt(7),rs.getString(8));
+
+                }
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return outputString;
     }
 }
