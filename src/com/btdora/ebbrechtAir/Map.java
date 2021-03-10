5a3e3a4cf4e717cd9fa19cbf3a34aba558769be2
T109P19A <hi@12.de>
2021-03-10 09:32:07 +0100

    Commit

--- src/com/btdora/ebbrechtAir/Map.java
+++ src/com/btdora/ebbrechtAir/Map.java
@@ -1,9 +1,7 @@
 package com.btdora.ebbrechtAir;
 
 
-import com.btdora.ebbrechtAir.classes.Fix;
-import com.btdora.ebbrechtAir.classes.GeoCoordinate;
-import com.btdora.ebbrechtAir.classes.Navaid;
+import com.sun.xml.internal.bind.v2.runtime.Coordinator;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
@@ -15,15 +13,8 @@ import java.util.ArrayList;
 public class Map extends Canvas {
     private GraphicsContext context = this.getGraphicsContext2D();
 
-
     String imagePath = "com/btdora/ebbrechtAir/images/";
     Image airport = new Image(imagePath + "airport.png");
-    Image navaids_dme = new Image(imagePath + "NavaidS_DME.png");
-    Image navaids_ndb = new Image(imagePath + "navaids_ndb.png");
-    Image navaids_vor = new Image(imagePath + "navaids_vor.png");
-    Image navaids_vordme = new Image(imagePath + "navaids_vordme.png");
-    Image fix = new Image(imagePath + "Fix.png");
-    //Image fix = new Image(imagePath + "fix.png");
 
     static private double canvasMidFactorX = 500;
     static private double canvasMidFactorY = 500;
@@ -42,70 +33,17 @@ public class Map extends Canvas {
     public Map() {
         super(1000, 1000);
 
-
-        Testdaten td1 = new Testdaten();
-        td1.airportsTest();
-
-
-    if(td1.objects.size() > 0){
-        for (int i = 0; i < td1.objects.size() ; i++){
-
-            if(td1.objects.get(i) instanceof Airport ){
-                double lat = td1.objects.get(i).getLat();
-                double lon = td1.objects.get(i).getLon();
-                drawairport(lat,lon);
-            }
-            else if(td1.objects.get(i) instanceof Fix ){
-                double lat = td1.objects.get(i).getLat();
-                double lon = td1.objects.get(i).getLon();
-                drawfix(lat,lon);
-            }
-            else if(td1.objects.get(i) instanceof Navaid){
-                double lat = td1.objects.get(i).getLat();
-                double lon = td1.objects.get(i).getLon();
-                drawNavaids_DME(lat,lon);
-            }
-            
-            
-
-        }
-    }
-
-
-
-
-
-
-
-
-
-
+        jumpToMapSection(zoomOnSeamiles(108),8.570456,50.033306);
+//        jumpToMapSection(1000,8.570456,50.033306);
+//        jumpToMapSection(60000,8.570456,50.033306);
 
         this.drawGrit();
-//        drawTestAirways();
-        drawairport(300,300);
-        drawNavaids_DME(300,300);
-//        Fix f1 = new Fix();
-//        f1.Fix();
 
-//        double lat2 = zoomdragFactorLat(td1.airportsArray.get(7).latitude, zoomFactor, offsetY, canvasMidFactorY);
-//        double lon2 = zoomdragFactorLon(td1.airportsArray.get(7).longitude, zoomFactor, offsetX, canvasMidFactorX);
-//        double lat3 = zoomdragFactorLat(td1.airportsArray.get(8).latitude, zoomFactor, offsetY, canvasMidFactorY);
-//        double lon3 = zoomdragFactorLon(td1.airportsArray.get(8).longitude, zoomFactor, offsetX, canvasMidFactorX);
-//        colorAirwayRoutes(lat2, lon2, lat3, lon3);
-//        for (int i = 0; i < td1.airportsArray.size(); i = i+2){
-//            double lat = zoomdragFactorLat(td1.airportsArray.get(i).latitude, zoomFactor, offsetY, canvasMidFactorY);
-//            double lon = zoomdragFactorLon(td1.airportsArray.get(i).longitude, zoomFactor, offsetX, canvasMidFactorX);
-//            double lat1 = zoomdragFactorLat(td1.airportsArray.get(i+1).latitude, zoomFactor, offsetY, canvasMidFactorY);
-//            double lon1 = zoomdragFactorLon(td1.airportsArray.get(i+1).longitude, zoomFactor, offsetX, canvasMidFactorX);
-//            setAirportLocation(lat, lon);
-//            setAirportLocation(lat1, lon1);
-//            drawAirwayLines(lat,lon,lat1,lon1);
-//
-//
-//        }
+        manageTestdrawing();
 
-
+        /**
+         * Scrolls on map towards mouse-position.
+         */
         setOnScroll (event -> {
             px1 = event.getX();
             py1 = event.getY();
@@ -123,33 +61,18 @@ public class Map extends Canvas {
                 offsetY = offsetY + ((canvasMidFactorY - py1)/5);
                 context.clearRect(0,0, 1000, 1000);
 
-                drawGrit();
-                td1.airportsTest();
-                double lat4 = zoomdragFactorLat(td1.airportsArray.get(7).latitude, zoomFactor, offsetY, canvasMidFactorY);
-                double lon4 = zoomdragFactorLon(td1.airportsArray.get(7).longitude, zoomFactor, offsetX, canvasMidFactorX);
-                double lat5 = zoomdragFactorLat(td1.airportsArray.get(8).latitude, zoomFactor, offsetY, canvasMidFactorY);
-                double lon5 = zoomdragFactorLon(td1.airportsArray.get(8).longitude, zoomFactor, offsetX, canvasMidFactorX);
-                colorAirwayRoutes(lat4, lon4, lat5, lon5);
-                for (int i = 0; i < td1.airportsArray.size(); i = i+2){
-                    double lat = zoomdragFactorLat(td1.airportsArray.get(i).latitude, zoomFactor, offsetY, canvasMidFactorY);
-                    double lon = zoomdragFactorLon(td1.airportsArray.get(i).longitude, zoomFactor, offsetX, canvasMidFactorX);
-                    double lat1 = zoomdragFactorLat(td1.airportsArray.get(i+1).latitude, zoomFactor, offsetY, canvasMidFactorY);
-                    double lon1 = zoomdragFactorLon(td1.airportsArray.get(i+1).longitude, zoomFactor, offsetX, canvasMidFactorX);
-                    setAirportLocation(lat, lon);
-                    setAirportLocation(lat1, lon1);
-                    drawAirwayLines(lat,lon,lat1,lon1);
-                }
+                manageTestdrawing();
             }
         });
-
 
         setOnMousePressed( event -> {
             this.px1 = event.getX();
             this.py1 = event.getY();
-            getLeftUpperCornerX();
-            getLeftUpperCornerY();
-            getRightLowerCornerX();
-            getRightLowerCornerY();
+            System.out.println(getLeftUpperCornerX());
+            System.out.println(getLeftUpperCornerY());
+            System.out.println(getRightLowerCornerX());
+            System.out.println(getRightLowerCornerY());
+            System.out.println(getSeamiles());
         });
         /**
          * Drags the points on the map.
@@ -162,41 +85,99 @@ public class Map extends Canvas {
             px1 = px2;
             py1 = py2;
 
-            context.clearRect(0,0, 1000, 1000);
-            drawGrit();
-            td1.airportsTest();
-            double lat6 = zoomdragFactorLat(td1.airportsArray.get(7).latitude, zoomFactor, offsetY, canvasMidFactorY);
-            double lon6 = zoomdragFactorLon(td1.airportsArray.get(7).longitude, zoomFactor, offsetX, canvasMidFactorX);
-            double lat7 = zoomdragFactorLat(td1.airportsArray.get(8).latitude, zoomFactor, offsetY, canvasMidFactorY);
-            double lon7 = zoomdragFactorLon(td1.airportsArray.get(8).longitude, zoomFactor, offsetX, canvasMidFactorX);
-            colorAirwayRoutes(lat6, lon6, lat7, lon7);
-            for (int i = 0; i < td1.airportsArray.size(); i = i+2){
-                double lat = zoomdragFactorLat(td1.airportsArray.get(i).latitude, zoomFactor, offsetY, canvasMidFactorY);
-                double lon = zoomdragFactorLon(td1.airportsArray.get(i).longitude, zoomFactor, offsetX, canvasMidFactorX);
-                double lat1 = zoomdragFactorLat(td1.airportsArray.get(i+1).latitude, zoomFactor, offsetY, canvasMidFactorY);
-                double lon1 = zoomdragFactorLon(td1.airportsArray.get(i+1).longitude, zoomFactor, offsetX, canvasMidFactorX);
-                setAirportLocation(lat, lon);
-                setAirportLocation(lat1, lon1);
-                drawAirwayLines(lat,lon,lat1,lon1);
-            }
+            manageTestdrawing();
         });
     }
 
-    public void getLeftUpperCornerX(){
-        leftUpperCornerX = offsetX - (500 * zoomFactor);
-        System.out.println(leftUpperCornerX);
+    public double getLeftUpperCornerX(){
+        leftUpperCornerX = ((offsetX + 500) / zoomFactor)*-1;
+        return leftUpperCornerX;
     }
-    public void getLeftUpperCornerY(){
-        leftUpperCornerY = offsetY + (500 * zoomFactor);
-        System.out.println(leftUpperCornerY);
+    public double getLeftUpperCornerY(){
+        leftUpperCornerY = (offsetY + 500) / zoomFactor;
+        return leftUpperCornerY;
     }
-    public void getRightLowerCornerX(){
-        rightLowerCornerX = offsetX + (500 * zoomFactor);
-        System.out.println(rightLowerCornerX);
+    public double getRightLowerCornerX(){
+        rightLowerCornerX = ((offsetX - 500) / zoomFactor)*-1;
+        return rightLowerCornerX;
     }
-    public void getRightLowerCornerY(){
-        rightLowerCornerY = offsetY - (500 * zoomFactor);
-        System.out.println(rightLowerCornerY);
+    public double getRightLowerCornerY(){
+        rightLowerCornerY = (offsetY - 500) / zoomFactor;
+        return rightLowerCornerY;
+    }
+
+    /**
+     * Jumps to a specific area on the map.
+     * @param zoom
+     * @param xCoordinate
+     * @param yCoordinate
+     */
+    public void jumpToMapSection(double zoom, double xCoordinate, double yCoordinate){
+        this.zoomFactor = zoom;
+        offsetX = manipulateX(xCoordinate);
+        offsetY = manipulateY(yCoordinate);
+    }
+
+    /**
+     * This method measures the width and height of the currently shown mapsection in seamiles.
+     * @return currentSeamiles
+     */
+    public double getSeamiles (){
+        double x1 = getLeftUpperCornerX();
+        double x2 = getRightLowerCornerX();
+        double currentDegreeRange = (x2-x1);
+        double currentSeamiles = currentDegreeRange*60;
+        return currentSeamiles;
+    }
+
+    /**
+     * Takes the amount of seamiles and returns the zoomfactor needed to zoom into the map to the given range.
+     * @param seamiles
+     * @return newZoomFactor
+     */
+    public double zoomOnSeamiles (double seamiles){
+        double newZoomFactor = 60000 / seamiles;
+        return newZoomFactor;
+    }
+
+    /**
+     * Ajusts x-coordinate to current map.
+     * @param x
+     * @return double x
+     */
+    public double manipulateX (double x){
+        x = x * -1 * this.zoomFactor;
+        return x;
+    }
+
+    /**
+     * Ajusts y-coordinate to current map.
+     * @param y
+     * @return double y
+     */
+    public double manipulateY (double y){
+        y = y * this.zoomFactor;
+        return y;
+    }
+
+    /**
+     * Draws testdata on map.
+     */
+    public void manageTestdrawing(){
+        Testdaten td1 = new Testdaten();
+        context.clearRect(0,0, 1000, 1000);
+        drawGrit();
+        td1.airportsTest();
+        double lat6 = zoomdragFactorLat(td1.airportsArray.get(7).latitude, zoomFactor, offsetY, canvasMidFactorY);
+        double lon6 = zoomdragFactorLon(td1.airportsArray.get(7).longitude, zoomFactor, offsetX, canvasMidFactorX);
+        double lat7 = zoomdragFactorLat(td1.airportsArray.get(8).latitude, zoomFactor, offsetY, canvasMidFactorY);
+        double lon7 = zoomdragFactorLon(td1.airportsArray.get(8).longitude, zoomFactor, offsetX, canvasMidFactorX);
+        colorAirwayRoutes(lat6, lon6, lat7, lon7);
+        for (int i = 0; i < td1.airportsArray.size(); i++){
+            double lat = zoomdragFactorLat(td1.airportsArray.get(i).latitude, zoomFactor, offsetY, canvasMidFactorY);
+            double lon = zoomdragFactorLon(td1.airportsArray.get(i).longitude, zoomFactor, offsetX, canvasMidFactorX);
+            setAirportLocation(lat, lon);
+        }
     }
 
     /**
@@ -234,24 +215,6 @@ public class Map extends Canvas {
         context.strokeLine(x1, y1, x2, y2);
     }
 
-//    /**
-//     * Processes and provides testcoordinates to draw on canvas.
-//     */
-//    public void drawTestAirways(){
-//        for (int i = 1; i < 30; i++) {
-//            lineCoordinates[0] = (i*10);
-//            lineCoordinates[1] = (i*20);
-//            lineCoordinates[2] = (i*30);
-//            lineCoordinates[3] = (i*40);
-//            drawAirwayLines(lineCoordinates[0],lineCoordinates[1],lineCoordinates[2],lineCoordinates[3]);
-//        }
-//        lineCoordinates[0] = (30*10);
-//        lineCoordinates[1] = (30*20);
-//        lineCoordinates[2] = (30*30);
-//        lineCoordinates[3] = (30*40);
-//        colorAirwayRoutes(lineCoordinates[0],lineCoordinates[1],lineCoordinates[2],lineCoordinates[3]);
-//    }
-
     /**
      * Draws all airway-lines on canvas.
      * @param lat1
@@ -284,11 +247,10 @@ public class Map extends Canvas {
      * @param zoomFactor
      * @param offsetY
      * @param canvasMidFactorY
-     * @return
+     * @return double lat
      */
     public double zoomdragFactorLat(double lat, double zoomFactor, double offsetY, double canvasMidFactorY){
-        lat = lat * this.zoomFactor * -1;
-        lat = lat + this.offsetY + this.canvasMidFactorY;
+        lat = manipulateX(lat) + this.offsetY + this.canvasMidFactorY;
         return lat;
     }
 
@@ -298,11 +260,10 @@ public class Map extends Canvas {
      * @param zoomFactor
      * @param offsetX
      * @param canvasMidFactorX
-     * @return
+     * @return double lon
      */
     public double zoomdragFactorLon(double lon, double zoomFactor, double offsetX, double canvasMidFactorX){
-        lon = lon * this.zoomFactor;
-        lon = lon + this.offsetX + this.canvasMidFactorX;
+        lon = manipulateY(lon) + this.offsetX + this.canvasMidFactorX;
         return lon;
     }
 
@@ -317,52 +278,5 @@ public class Map extends Canvas {
         lon = lon - measurements / 2;
         context.drawImage(airport, lon, lat, measurements, measurements);
     }
-
-
-    public void drawairport(double lat, double lon){
-        int measurements = 50;
-        lat = lat - measurements / 2;
-        lon = lon - measurements / 2;
-        context.drawImage(airport, lon, lat, measurements, measurements);
-    }
-
-    public void drawNavaids_DME(double lat, double lon){
-        int measurements = 50;
-        lat = lat - measurements / 2;
-        lon = lon - measurements / 2;
-        context.drawImage(navaids_dme, lon, lat, measurements, measurements);
-    }
-
-    public void drawNavaids_ndb(double lat, double lon){
-        int measurements = 50;
-        lat = lat - measurements / 2;
-        lon = lon - measurements / 2;
-        context.drawImage(navaids_ndb, lon, lat, measurements, measurements);
-    }
-
-
-    public void drawNavaids_vor(double lat, double lon){
-        int measurements = 50;
-        lat = lat - measurements / 2;
-        lon = lon - measurements / 2;
-        context.drawImage(navaids_vor, lon, lat, measurements, measurements);
-    }
-
-
-    public void drawNavaids_vordme(double lat, double lon){
-        int measurements = 50;
-        lat = lat - measurements / 2;
-        lon = lon - measurements / 2;
-        context.drawImage(navaids_vordme, lon, lat, measurements, measurements);
-    }
-
-
-    public void drawfix(double lat, double lon){
-        int measurements = 50;
-        lat = lat - measurements / 2;
-        lon = lon - measurements / 2;
-        context.drawImage(fix, lon, lat, measurements, measurements);
-    }
-
 }
 
