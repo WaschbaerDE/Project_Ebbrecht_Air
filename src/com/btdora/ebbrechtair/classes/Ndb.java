dcaa1758ae127bd1b0477667d65f5b365e1d1a17
Adrian <adrianpokemon@live.de>
2021-03-11 15:34:28 +0100

    Refracting der Attribute und Methoden in der Klasse NDB

--- src/com/btdora/ebbrechtair/classes/Ndb.java
+++ src/com/btdora/ebbrechtair/classes/Ndb.java
@@ -1,5 +1,9 @@
 package com.btdora.ebbrechtair.classes;
 
+//Klasse definiert einen Navaid als NDB
+//NON-DIRECTIONAL BEACON
+//NDB = Kreisfunkfeuer. Sendeanlage die Funkwellen ausstrahlt.
+
 public class Ndb extends Navaid {
     public Ndb(String navaidID, String navaidName, Double frequency, double lat ,double lon, int altitude, String areaCode) {
         super(navaidID, navaidName, frequency, 0, 0,lat,lon,altitude,null);
