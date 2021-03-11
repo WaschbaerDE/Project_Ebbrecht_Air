56d51b2773a6d35ff56c1dfeef2ae455342a351b
Adrian <adrianpokemon@live.de>
2021-03-11 15:27:17 +0100

    Refracting der Attribute und Methoden in der Klasse Dme

--- src/com/btdora/ebbrechtair/classes/Dme.java
+++ src/com/btdora/ebbrechtair/classes/Dme.java
@@ -1,5 +1,10 @@
 package com.btdora.ebbrechtair.classes;
 
+
+// Überprüft ob es sich bei der Navaid um ein DME handelt
+// DME  ist eine Funkstelle des Navigationsfunkdienstes, deren Aussendungen dazu bestimmt sind, einem
+// Luftfahrzeug die Feststellung seiner Entfernung in Bezug auf einen ortsfesten Referenzpunkt zu ermöglichen.
+
 public class Dme extends Navaid {
     public Dme(String navaidID, String navaidName, Double frequency, double lat, double lon, int altitude, String areaCode) {
         super(navaidID, navaidName, frequency, 0, 1, lat, lon, altitude, areaCode);
